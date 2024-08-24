6401d72b4ab186f5857aa0ecc4478b5dfa3885fd
21aru <21aru@Champ.mshome.net>
2024-08-24 11:12:42 +0530

    First Commit

---
+++ src/com/git/Captain.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class Captain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 21aru
+ *
+ */
+module Demo_GIT {
+}
